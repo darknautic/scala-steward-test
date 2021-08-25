75eaaf21a2f6f07bafe562208ebe59e28e09f60c
darknautic <nautikos@codeinmotion.org>
2021-08-25 16:46:00 -0400

    initial commit

---
+++ src/main/scala/org/codeinmotion/Main.scala
@@ -0,0 +1,11 @@
+package org.codeinmotion
+
+import cats.effect.IOApp
+import cats.effect.IO
+
+object Main extends IOApp.Simple {
+
+  // This is your new "main"!
+  def run: IO[Unit] =
+    HelloWorld.say().flatMap(IO.println)
+}
